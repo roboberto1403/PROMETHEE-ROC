0487cb69ee02aca5b8a0ad33b4e93b47969aec11
Luiz Roberto <lrbf@cin.ufpe.br>
2024-03-21 13:36:58 -0300

    updated setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='promethee_roc',
+    name='promethee_ROC',
     version='0.1',
     packages=find_packages(),
     author='Luiz Roberto, Penélope Araújo, Luana Cristina, Gabrielle Almeida, João Felipe',
     author_email='lrbf@cin.ufpe.br, pmpa@cin.ufpe.br, lccb@cin.ufpe.br, gao2@cin.ufpe.br, jfbs@cin.ufpe.br',
     description='Uma biblioteca para realizar análises de problema multicritérios com o método PROMETHEE utilizando pesos substitutos com o método ROC',
-    url='https://github.com/roboberto1403/PROMETHEE-ROC',
+    url='https://github.com/roboberto1403/promethee_ROC',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
